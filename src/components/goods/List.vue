da1d7a60173060d720f263a24048ea0db2f1244d
memory1668 <1163549075@qq.com>
2019-10-12 10:27:59 +0800

    完成商品列表和添加商品功能

---
+++ src/components/goods/List.vue
@@ -0,0 +1,138 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片区域 -->
+    <el-card>
+      <!-- 搜索和添加商品按钮 -->
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input
+            placeholder="请输入内容"
+            v-model="queryInfo.query"
+            clearable
+            @clear="getGoodsList"
+            @keyup.enter.native="getGoodsList"
+          >
+            <el-button slot="append" icon="el-icon-search" @click="getGoodsList"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="goAddPage">添加商品</el-button>
+        </el-col>
+      </el-row>
+      <!-- 商品表格区域 -->
+      <el-table :data="goodsList" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+        <el-table-column label="商品价格(元)" prop="goods_price" width="95px"></el-table-column>
+        <el-table-column label="商品重量" prop="goods_weight" width="70px"></el-table-column>
+        <el-table-column label="创建时间" width="140px">
+          <template v-slot="slotProps">{{slotProps.row.add_time | dateFormat}}</template>
+        </el-table-column>
+        <el-table-column label="操作" width="120px">
+          <template v-slot="slotProps">
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              size="mini"
+              @click="removeGoods(slotProps.row.goods_id)"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 20, 30]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        background
+      ></el-pagination>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 查询参数对象
+      queryInfo: {
+        // 查询参数
+        query: '',
+        // 当前页码
+        pagenum: 1,
+        // 每页显示条数
+        pagesize: 10
+      },
+      // 商品列表数据
+      goodsList: [],
+      // 商品列表总数
+      total: 0
+    }
+  },
+  created() {
+    this.getGoodsList()
+  },
+  methods: {
+    // 获取商品列表
+    async getGoodsList() {
+      const { data: res } = await this.$http.get('goods', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品列表失败')
+      }
+      console.log(res.data)
+      this.goodsList = res.data.goods
+      this.total = res.data.total
+    },
+    // 分页大小发生变化
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      // 重新获取商品列表
+      this.getGoodsList()
+    },
+    // 当前页码发生变化
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      // 重新获取商品列表
+      this.getGoodsList()
+    },
+    // 删除商品（数据库操作）
+    async removeGoods(id) {
+      const confirmResult = await this.$confirm('此操作将永久删除该商品, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err=>err)
+      if(confirmResult !== 'confirm'){
+        return this.$message.info('已取消删除')
+      }
+      const {data:res} = await this.$http.delete('goods/' + id)
+      if(res.meta.status !== 200){
+        return this.$message.error('删除商品失败')
+      }
+      // 重新获取商品列表
+      this.getGoodsList()
+      this.$message.success('删除商品成功')
+    },
+    // 通过编程式导航跳转到添加商品组件
+    goAddPage(){
+      this.$router.push('/goods/add')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
